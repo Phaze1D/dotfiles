03595dd788b84e94d91cde68668ab498e1dd5660
David Villarreal <davidvillarreal@Davids-MacBook-Air.local>
2023-04-11 21:57:35 -0600

    Test

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { 
+        "javascript", 
+        "typescript", 
+        "tsx", 
+        "graphql", 
+        "json", 
+        "dockerfile", 
+        "css", 
+        "lua", 
+        "html",
+    },
+    sync_install = false,
+    auto_install = false,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+  })
