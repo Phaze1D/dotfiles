2414224a3ec63c9a2c7992721fb73422b41010c3
David Villarreal <davidvillarreal@Davids-MacBook-Air.local>
2023-04-17 13:11:16 -0600

    wip

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,31 @@
-require'lspconfig'.tsserver.setup({})
-require'lspconfig'.graphql.setup({
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({
+    buffer = bufnr,
+    preserve_mappings = false,
+  })
+end)
+
+require('lspconfig').tsserver.setup({})
+require('lspconfig').graphql.setup({
   filetypes = { "graphql", "gql", "typescript", "javascript", "typescriptreact", "javascriptreact" }
+})
+
+
+lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'buffer', keyword_length = 2},
+  },
+  mappings = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }
 })
