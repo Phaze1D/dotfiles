8dc63b23956006e9a07d62343eac9c4a20265af4
David Villarreal <davidvillarreal@Davids-MacBook-Air.local>
2023-04-17 19:36:04 -0600

    Added lsp and cmp

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -8,24 +8,54 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 require('lspconfig').tsserver.setup({})
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 require('lspconfig').graphql.setup({
   filetypes = { "graphql", "gql", "typescript", "javascript", "typescriptreact", "javascriptreact" }
 })
 
-
-lsp.setup()
-
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+require('lsp-zero').cmp_action()
+
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
   sources = {
     {name = 'path'},
     {name = 'nvim_lsp'},
     {name = 'buffer', keyword_length = 2},
   },
-  mappings = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }
+  mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
 })
+
+lsp.setup()
