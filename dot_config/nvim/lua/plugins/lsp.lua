025a6640e41c93378a45b5ff0cd89c53a67f653e
David Villarreal <steadypathapp@gmail.com>
2023-04-17 19:45:15 -0600

    wip

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -8,12 +8,12 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lspconfig.tsserver.setup({})
+lspconfig.dockerls.setup({})
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
 lspconfig.graphql.setup({
   filetypes = { "graphql", "gql", "typescript", "javascript", "typescriptreact", "javascriptreact" }
 })
 lspconfig.eslint.setup({
-  --- ...
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
