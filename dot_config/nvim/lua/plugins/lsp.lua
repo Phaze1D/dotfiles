65fbbd0968a1eec10261bb10a0f59d4e9489cfed
David Villarreal <steadypathapp@gmail.com>
2025-05-07 16:19:48 -0600

    updating setup

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,37 @@
-local lsp = require('lsp-zero').preset({})
 local lspconfig = require('lspconfig')
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({
-    buffer = bufnr,
-    preserve_mappings = false,
-  })
-end)
+
+vim.opt.signcolumn = 'yes'
+
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- This is where you enable features that only work
+-- if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
 
 lspconfig.ts_ls.setup({})
 lspconfig.dockerls.setup({})
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+lspconfig.lua_ls.setup({})
 lspconfig.graphql.setup({
   filetypes = { "graphql", "gql", "typescript", "javascript", "typescriptreact", "javascriptreact" }
 })
@@ -21,4 +43,3 @@ lspconfig.eslint.setup({
     })
   end,
 })
-lsp.setup()
