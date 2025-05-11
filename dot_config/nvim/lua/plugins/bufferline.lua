3e09ed34aad7ef35be248825332098fff2854448
David Villarreal <steadypathapp@gmail.com>
2025-05-11 17:55:07 -0600

    refactor(bufferline): replace cmd abbreviation with keymaps
    Replace command abbreviation for buffer close with explicit keymappings:
    - Remove 'db' abbreviation for BufferLinePickClose
    - Add '<leader>bp' for BufferLinePick
    - Add '<leader>bd' for buffer deletion

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -15,4 +15,5 @@ require("bufferline").setup({
 local opts = { noremap = true }
 vim.keymap.set('n', '<TAB>', ':BufferLineCycleNext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', opts)
-vim.cmd([[cnoreabbrev db BufferLinePickClose]])
+vim.keymap.set('n', '<leader>bp', ':BufferLinePick<CR>', opts)
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', opts)
