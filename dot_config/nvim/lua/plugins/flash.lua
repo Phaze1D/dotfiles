2efd469dbecddf5833a9d21185c7ca002abdff9d
David Villarreal <davidvillarreal@Davids-MacBook-Pro.local>
2025-05-14 12:13:43 -0600

    feat(nvim/flash): add word-start search to Flash jump keybinding

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -7,7 +7,15 @@ flash.setup({
   },
 })
 
-vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { desc = "Flash", noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "s", function()
+  flash.jump({
+    search = {
+      mode = function(str)
+        return "\\<" .. str
+      end,
+    },
+  })
+end, { desc = "Flash", noremap = true })
 vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { desc = "Flash Treesitter", noremap = true })
 vim.keymap.set("o", "r", flash.remote, { desc = "Remote Flash", noremap = true })
 vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "Treesitter Search", noremap = true })
