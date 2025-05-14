fa5e50e3768eb28555942817550484e7a3775443
David Villarreal <steadypathapp@gmail.com>
2025-05-14 11:59:37 -0600

    feat(nvim): update flash.lua config with jump labels and optimize keymaps
    Enable jump_labels for char mode in flash.lua for better navigation and refactor
    keymap definitions to use direct function references instead of anonymous functions
    for improved performance and cleaner code.

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -1,8 +1,14 @@
 local flash = require('flash')
-flash.setup()
+flash.setup({
+  modes = {
+    char = {
+      jump_labels = true
+    },
+  },
+})
 
-vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
-vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
-vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
-vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
-vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
+vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { desc = "Flash", noremap = true })
+vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { desc = "Flash Treesitter", noremap = true })
+vim.keymap.set("o", "r", flash.remote, { desc = "Remote Flash", noremap = true })
+vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "Treesitter Search", noremap = true })
+vim.keymap.set("c", "<c-s>", flash.toggle, { desc = "Toggle Flash Search", noremap = true })
