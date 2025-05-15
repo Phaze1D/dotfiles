dc5a1311aced5fb2d2e132fbf715128b20c0b5bb
David Villarreal <steadypathapp@gmail.com>
2025-05-15 17:01:58 -0600

    chore: update LSP keybindings and Neovim configuration
    - Update LSP keybindings in lsp.lua and snacks.lua
      - Changed gr to rename and gR for references
      - Map ga to code_action (replacing <F4>)
      - Removed redundant keybindings in lsp.lua as they are handled by snacks.lua
    - Remove nvim-notify plugin dependency
    - Add .luarc.json for better Lua language server configuration

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -74,7 +74,7 @@ vim.keymap.set("n", "<leader>uC", function() Snacks.picker.colorschemes() end, {
 -- LSP
 vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "Goto Definition" })
 vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, { desc = "Goto Declaration" })
-vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { desc = "References", nowait = true })
+vim.keymap.set("n", "gR", function() Snacks.picker.lsp_references() end, { desc = "References", nowait = true })
 vim.keymap.set("n", "gI", function() Snacks.picker.lsp_implementations() end, { desc = "Goto Implementation" })
 vim.keymap.set("n", "gy", function() Snacks.picker.lsp_type_definitions() end, { desc = "Goto T[y]pe Definition" })
 vim.keymap.set("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
