61edd29ed65ed735dbb8ee75b2296bd9f8f57718
David Villarreal <steadypathapp@gmail.com>
2025-05-16 08:40:22 -0600

    feat(nvim): disable mini.diff by default
    Configure mini.diff to use 'none' source. This disables automatic
    diffing behavior by default, allowing more controlled usage when needed.

--- dot_config/nvim/lua/plugins/mini-diff.lua
+++ dot_config/nvim/lua/plugins/mini-diff.lua
@@ -1,4 +1,10 @@
 return {
   'echasnovski/mini.diff',
   version = '*',
+  config = function()
+    local diff = require("mini.diff")
+    diff.setup {
+      source = diff.gen_source.none(),
+    }
+  end,
 }
