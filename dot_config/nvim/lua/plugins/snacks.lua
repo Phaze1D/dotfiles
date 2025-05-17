fb917844402c8b617ee5b24bafa454b1d7018647
David Villarreal <steadypathapp@gmail.com>
2025-05-17 14:34:10 -0600

    fix(ui): fix borders in blink plugin and simplify key mappings
    Fix misplaced border configuration in blink plugin and add border to list
    window. Also add diffview plugin and simplify the key mapping definitions
    in snacks.lua by using a consistent formatting style.

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -64,44 +64,30 @@ return {
     { "<leader>sa",      function() Snacks.picker.autocmds() end,                                desc = "Autocmds" },
     -- "<leader>sb" is already defined above
     { "<leader>sC",      function() Snacks.picker.command_history() end,                         desc = "Command History" },
-    {
-      "<leader>sc",
-      function()
-        Snacks.picker.commands({
-          finder = "vim_commands",
-          format = "command",
-          preview = "preview",
-          confirm = function(picker, item)
-            picker:close()
-            vim.cmd(item.cmd)
-          end
-        })
-      end,
-      desc = "Commands"
-    },
-    { "<leader>sd", function() Snacks.picker.diagnostics() end,           desc = "Diagnostics" },
-    { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end,    desc = "Buffer Diagnostics" },
-    { "<leader>sh", function() Snacks.picker.help() end,                  desc = "Help Pages" },
-    { "<leader>sH", function() Snacks.picker.highlights() end,            desc = "Highlights" },
-    { "<leader>si", function() Snacks.picker.icons() end,                 desc = "Icons" },
-    { "<leader>sj", function() Snacks.picker.jumps() end,                 desc = "Jumps" },
-    { "<leader>sk", function() Snacks.picker.keymaps() end,               desc = "Keymaps" },
-    { "<leader>sl", function() Snacks.picker.loclist() end,               desc = "Location List" },
-    { "<leader>sm", function() Snacks.picker.marks() end,                 desc = "Marks" },
-    { "<leader>sM", function() Snacks.picker.man() end,                   desc = "Man Pages" },
-    { "<leader>sp", function() Snacks.picker.lazy() end,                  desc = "Search for Plugin Spec" },
-    { "<leader>sq", function() Snacks.picker.qflist() end,                desc = "Quickfix List" },
-    { "<leader>sR", function() Snacks.picker.resume() end,                desc = "Resume" },
-    { "<leader>su", function() Snacks.picker.undo() end,                  desc = "Undo History" },
-    { "<leader>uC", function() Snacks.picker.colorschemes() end,          desc = "Colorschemes" },
+    { "<leader>sc",      function() Snacks.picker.commands() end,                                desc = "Commands" },
+    { "<leader>sd",      function() Snacks.picker.diagnostics() end,                             desc = "Diagnostics" },
+    { "<leader>sD",      function() Snacks.picker.diagnostics_buffer() end,                      desc = "Buffer Diagnostics" },
+    { "<leader>sh",      function() Snacks.picker.help() end,                                    desc = "Help Pages" },
+    { "<leader>sH",      function() Snacks.picker.highlights() end,                              desc = "Highlights" },
+    { "<leader>si",      function() Snacks.picker.icons() end,                                   desc = "Icons" },
+    { "<leader>sj",      function() Snacks.picker.jumps() end,                                   desc = "Jumps" },
+    { "<leader>sk",      function() Snacks.picker.keymaps() end,                                 desc = "Keymaps" },
+    { "<leader>sl",      function() Snacks.picker.loclist() end,                                 desc = "Location List" },
+    { "<leader>sm",      function() Snacks.picker.marks() end,                                   desc = "Marks" },
+    { "<leader>sM",      function() Snacks.picker.man() end,                                     desc = "Man Pages" },
+    { "<leader>sp",      function() Snacks.picker.lazy() end,                                    desc = "Search for Plugin Spec" },
+    { "<leader>sq",      function() Snacks.picker.qflist() end,                                  desc = "Quickfix List" },
+    { "<leader>sR",      function() Snacks.picker.resume() end,                                  desc = "Resume" },
+    { "<leader>su",      function() Snacks.picker.undo() end,                                    desc = "Undo History" },
+    { "<leader>uC",      function() Snacks.picker.colorschemes() end,                            desc = "Colorschemes" },
 
     -- LSP
-    { "gd",         function() Snacks.picker.lsp_definitions() end,       desc = "Goto Definition" },
-    { "gD",         function() Snacks.picker.lsp_declarations() end,      desc = "Goto Declaration" },
-    { "gR",         function() Snacks.picker.lsp_references() end,        desc = "References",            nowait = true },
-    { "gI",         function() Snacks.picker.lsp_implementations() end,   desc = "Goto Implementation" },
-    { "gy",         function() Snacks.picker.lsp_type_definitions() end,  desc = "Goto T[y]pe Definition" },
-    { "<leader>ss", function() Snacks.picker.lsp_symbols() end,           desc = "LSP Symbols" },
-    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    { "gd",              function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
+    { "gD",              function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
+    { "gR",              function() Snacks.picker.lsp_references() end,                          desc = "References",               nowait = true },
+    { "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
+    { "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
+    { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
+    { "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
   }
 }
