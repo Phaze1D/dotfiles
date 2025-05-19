dbeff362aee3a7a86d9cc52a37dccb5ed35bdcca
David Villarreal <steadypathapp@gmail.com>
2025-05-19 11:24:48 -0600

    Change auto-save to use maintained version

--- dot_config/nvim/lua/plugins/auto-save.lua
+++ dot_config/nvim/lua/plugins/auto-save.lua
@@ -1,3 +1,21 @@
 return {
-  "Pocco81/auto-save.nvim",
+  "okuuva/auto-save.nvim",
+  version = '^1.0.0',
+  cmd = "ASToggle",
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  opts = {
+    enabled = true,
+    trigger_events = { -- See :h events
+      immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" },
+      defer_save = { "InsertLeave", "TextChanged" },
+      cancel_deferred_save = { "InsertEnter" },
+    },
+    condition = nil,
+    write_all_buffers = false, -- write all buffers when the current one meets `condition`
+    noautocmd = false,         -- do not execute autocmds when saving
+    lockmarks = false,         -- lock marks when saving, see `:h lockmarks` for more details
+    debounce_delay = 1000,     -- delay after which a pending save is executed
+    debug = false,
+
+  }
 }
