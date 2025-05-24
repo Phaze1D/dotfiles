c1c04358c477f90508956194ca99764ef01cc8d8
David Villarreal <steadypathapp@gmail.com>
2025-05-24 10:01:32 -0600

    refactor(lsp): modularize LSP configurations and improve code organization
    - Extract LSP configurations to separate files for maintainability
    - Create utility functions for keymaps and formatting setup
    - Add enhanced support for JSON language server
    - Improve ESLint configuration with detailed options
    - Fix Markdown formatting behavior to respect file type
    - Update TypeScript language server with better documentation

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,52 @@
+local function setupKeymaps(event)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
+    {
+      buffer = event.buf,
+      desc = 'Show signature help'
+    }
+  )
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<cr>',
+    {
+      buffer = event.buf,
+      desc = 'Rename symbol'
+    }
+  )
+  local filetype = vim.api.nvim_buf_get_option(event.buf, 'filetype')
+
+  if filetype ~= "codecompanion" then
+    vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+      {
+        buffer = event.buf,
+        desc = 'Code actions'
+      }
+    )
+  end
+end
+
+local function setupFormatting(event)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = event.buf,
+    callback = function()
+      local filetype = vim.api.nvim_buf_get_option(event.buf, 'filetype')
+      if filetype ~= "markdown" and filetype ~= "markdown.mdx" then
+        vim.cmd.undojoin()
+        vim.lsp.buf.format({ async = false })
+      end
+    end,
+  })
+end
+
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = { 'saghen/blink.cmp' },
   opts = {
     servers = {
       lua_ls = {},
-      ts_ls = {},
-      eslint = {},
-      marksman = {},
+      ts_ls = require('utils.lsp.ts'),
+      eslint = require('utils.lsp.eslint'),
+      marksman = require('utils.lsp.marksman'),
+      jsonls = require('utils.lsp.json'),
     }
   },
   config = function(_, opts)
@@ -20,40 +60,8 @@ return {
     vim.api.nvim_create_autocmd('LspAttach', {
       desc = 'LSP actions',
       callback = function(event)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
-          {
-            buffer = event.buf,
-            desc = 'Show signature help'
-          }
-        )
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<cr>',
-          {
-            buffer = event.buf,
-            desc = 'Rename symbol'
-          }
-        )
-        local filetype = vim.api.nvim_buf_get_option(event.buf, 'filetype')
-
-        -- Only set up code_action keybinding if not a CodeCompanion file
-        if filetype ~= "codecompanion" then
-          vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>',
-            {
-              buffer = event.buf,
-              desc = 'Code actions'
-            }
-          )
-        end
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = event.buf,
-          command = "LspEslintFixAll",
-          callback = function()
-            if filetype ~= "markdown" then
-              vim.cmd.undojoin()
-              vim.lsp.buf.format({ async = false })
-            end
-          end,
-        })
+        setupKeymaps(event)
+        setupFormatting(event)
       end,
     })
   end,
