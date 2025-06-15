f2d2f5c606d431a7bf117d6cab703e58fadb2f09
David Villarreal <steadypathapp@gmail.com>
2025-06-15 12:15:34 -0600

    removed print

--- dot_config/nvim/lua/plugins/lsp-format.lua
+++ dot_config/nvim/lua/plugins/lsp-format.lua
@@ -4,8 +4,6 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        print("LSP client attached: " .. client.name)
-
         if client.name ~= "ts_ls" then
           require("lsp-format").on_attach(client, args.buf)
         end
