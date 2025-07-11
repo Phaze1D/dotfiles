f9a7836051cc389f71ef652ed058b1da63e5d705
David Villarreal <steadypathapp@gmail.com>
2025-07-10 19:22:28 -0600

    feat(nvim): switch colorscheme from onedark to tokyonight
    - Change default install colorscheme in lazy.lua to tokyonight
    - Disable onedark plugin
    - Enable tokyonight plugin and set as active colorscheme
    - Add kanagawa plugin configuration (disabled)

--- dot_config/nvim/lua/plugins/onedark.lua
+++ dot_config/nvim/lua/plugins/onedark.lua
@@ -1,6 +1,7 @@
 return {
   "navarasu/onedark.nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
+  enabled = false,
   config = function()
     require('onedark').setup {
       style = 'deep'
