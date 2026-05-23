4d2945e66a97644955a71001c23ec0a6bbf9c038
David Villarreal <tribys@Davids-MacBook-Air.local>
2026-05-23 09:36:40 -0600

    Updated to new treesitter parsers

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,20 @@
 return {
- "nvim-treesitter/nvim-treesitter",
- event = { "BufReadPost", "BufNewFile" },
- branch = 'master',
- build = function()
-   require("nvim-treesitter.install").update({ with_sync = true })()
- end,
- config = function()
-   local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+  config = function()
+    local parsers = {
+      "yaml", "javascript", "typescript", "tsx", "dart",
+      "graphql", "json", "dockerfile", "css", "lua",
+      "sql", "html", "markdown", "markdown_inline", "prisma",
+    }
 
-   configs.setup({
-     ensure_installed = {
-       "yaml",
-       "javascript",
-       "typescript",
-       "tsx",
-       "dart",
-       "graphql",
-       "json",
-       "dockerfile",
-       "css",
-       "lua",
-       "sql",
-       "html",
-       "dart",
-       "markdown",
-       "markdown_inline",
-       "prisma",
-     },
-     sync_install = false,
-     auto_install = false,
-     ignore_install = { "gitcommit" },
-     highlight = {
-       enable = true,
-       additional_vim_regex_highlighting = false,
-     },
-   })
- end
+    require("nvim-treesitter").install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
+    })
+  end
 }
-
